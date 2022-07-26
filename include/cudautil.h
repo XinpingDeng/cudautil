ca22dc65fe614ba15b80be1ba63936099e87d5b7
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2022-07-26 11:15:12 +1000

    the first all header version

--- include/cudautil.h
+++ include/cudautil.h
@@ -574,4 +574,454 @@ private:
   T2 *d_data2 = NULL; ///< private variable to hold input vector pointer 2
 };
 
+//! A template kernel to build complex numbers with its real and imag part
+/*!
+ * 
+ * \see scalar_typecast
+ *
+ * \tparam TREAL Real part data type
+ * \tparam TIMAG Imag part data type
+ * \tparam TCMPX Complex data type
+ * 
+ * \param[in]  d_real  Real part to build complex numbers
+ * \param[in]  d_imag  Imag part to build complex numbers
+ * \param[in]  ndata   Number of data points ton be built
+ * \param[out] d_cmpx  Complex numbers
+ *
+ */
+template <typename TREAL, typename TIMAG, typename TCMPX>
+__global__ void cudautil_complexbuilder(const TREAL *d_real, const TIMAG *d_imag, TCMPX *d_cmpx, int ndata){
+  // Maximum x-dimension of a grid of thread blocks is 2^31-1
+  // Maximum x- or y-dimension of a block is 1024
+  // So here we can cover (2^31-1)*1024 random numbers, which are 2^41-1024
+  // should be big enough
+  
+  int idx = blockDim.x*blockIdx.x + threadIdx.x;
+  if(idx<ndata){
+
+    scalar_typecast(d_real[idx], d_cmpx[idx].x);
+    scalar_typecast(d_imag[idx], d_cmpx[idx].y);
+  }
+}
+
+/*! \brief A class to build a complex vector with two real vectors
+ *
+ * \tparam TREAL Typename of the real part data
+ * \tparam TIMAG Typename of the imag part data
+ * \tparam TCMPX Typename of the complex data
+ *
+ * The class use kernel `cudautil_complexbuilder` to convert data type and build complex numbers. `cudautil_complexbuilder` uses `scalar_typecast` to convert data type. 
+ * As of that, the allowed data type here is limited to following table (more types can be added later) 
+ * 
+ * TREAL/TIMAG    | TCMPX
+ * -------|----
+ * float  | cuComplex
+ * float  | cuDoubleComplex
+ * float  | half2
+ * float  | int2
+ * float  | short2
+ * float  | int8_t ???
+ * double | cuComplex
+ * half   | cuComplex
+ * int    | cuComplex
+ * int16_t| cuComplex
+ * int8_t | cuComplex
+ *
+ */
+template <typename TREAL, typename TIMAG, typename TCMPX>
+class ComplexDataBuilder {
+public:
+  TCMPX *d_cmpx = NULL; ///< Complex data on device
+  
+  //! Constructor of ComplexDataBuilder class.
+  /*!
+   * 
+   * - initialise the class
+   * - create device memory for \p ndata complex numbers
+   * - build complex numbers with \p d_real and \p d_imag
+   *
+   * \see cudautil_complexbuilder, scalar_typecast
+   *
+   * \tparam TREAL Real part data type
+   * \tparam TIMAG Imag part data type
+   * \tparam TCMPX Complex data type
+   * 
+   * \param[in] d_real  Real part to build complex numbers
+   * \param[in] d_imag  Imag part to build complex numbers
+   * \param[in] ndata   Number of data points ton be converted
+   * \param[in] nthread Number of threads per CUDA block to run `cudautil_complexbuilder` kernel
+   *
+   */
+  ComplexDataBuilder(TREAL *d_real, TIMAG *d_imag, int ndata, int nthread )
+    :d_real(d_real), d_imag(d_imag), ndata(ndata), nthread(nthread){
+    checkCudaErrors(cudaMalloc(&d_cmpx, ndata*sizeof(TCMPX)));
+
+    nblock = ndata/nthread;
+    nblock = (nblock>1)?nblock:1;
+    
+    cudautil_complexbuilder<<<nblock, nthread>>>(d_real, d_imag, d_cmpx, ndata);
+    getLastCudaError("Kernel execution failed [ cudautil_complexbuilder ]");
+  }
+  
+  //! Deconstructor of ComplexDataBuilder class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~ComplexDataBuilder(){
+    checkCudaErrors(cudaFree(d_cmpx));
+  }
+  
+private:
+  TREAL *d_real = NULL;
+  TIMAG *d_imag = NULL;
+  
+  int ndata;
+  int nthread;
+  int nblock;
+};
+
+//! A template kernel to split complex numbers into its real and imag part
+/*!
+ * 
+ * \see scalar_typecast
+ *
+ * \tparam TREAL Real part data type
+ * \tparam TIMAG Imag part data type
+ * \tparam TCMPX Complex data type
+ * 
+ * \param[in]  d_cmpx  Complex numbers 
+ * \param[out] d_real  Real part of complex numbers
+ * \param[out] d_imag  Imag part of complex numbers
+ * \param[in]  ndata   Number of data points to be splitted
+ *
+ */
+template <typename TCMPX, typename TREAL, typename TIMAG>
+__global__ void cudautil_complexsplitter(const TCMPX *d_cmpx, const TREAL *d_real, TIMAG *d_imag, int ndata){
+  // Maximum x-dimension of a grid of thread blocks is 2^31-1
+  // Maximum x- or y-dimension of a block is 1024
+  // So here we can cover (2^31-1)*1024 random numbers, which are 2^41-1024
+  // should be big enough
+  
+  int idx = blockDim.x*blockIdx.x + threadIdx.x;
+  if(idx<ndata){
+
+    scalar_typecast(d_cmpx[idx].x,   d_real[idx]);
+    scalar_typecast(d_cmpx[idx].y, d_imag[idx]);
+  }
+}
+
+/*! \brief A class to build a complex vector with two real vectors
+ *
+ * \tparam TREAL Typename of the real part data
+ * \tparam TIMAG Typename of the imag part data
+ * \tparam TCMPX Typename of the complex data
+ *
+ * The class use kernel `cudautil_complexbuilder` to convert data type and build complex numbers. `cudautil_complexbuilder` uses `scalar_typecast` to convert data type. 
+ * As of that, the allowed data type here is limited to following table (more types can be added later) 
+ * 
+ * TREAL/TIMAG    | TCMPX
+ * -------|----
+ * float  | cuComplex
+ * float  | cuDoubleComplex
+ * float  | half2
+ * float  | int2
+ * float  | short2
+ * float  | int8_t ???
+ * double | cuComplex
+ * half   | cuComplex
+ * int    | cuComplex
+ * int16_t| cuComplex
+ * int8_t | cuComplex
+ *
+ */
+template <typename TCMPX, typename TREAL, typename TIMAG>
+class ComplexDataSplitter {
+public:
+  TREAL *d_real = NULL; ///< Real part on device
+  TIMAG *d_imag = NULL; ///< Imag part on device
+  
+  //! Constructor of ComplexDataSplitter class.
+  /*!
+   * 
+   * - initialise the class
+   * - create device memory for \p ndata real and imag numbers
+   * - split complex numbers to \p d_real and \p d_imag
+   *
+   * \see cudautil_complexbuilder, scalar_typecast
+   *
+   * \tparam TREAL Real part data type
+   * \tparam TIMAG Imag part data type
+   * \tparam TCMPX Complex data type
+   * 
+   * \param[in] d_cmpx  Complex numbers
+   * \param[in] ndata   Number of data points ton be converted
+   * \param[in] nthread Number of threads per CUDA block to run `cudautil_complexbuilder` kernel
+   *
+   */
+  
+  ComplexDataSplitter(TCMPX *d_cmpx, int ndata, int nthread)
+    :d_cmpx(d_cmpx), ndata(ndata), nthread(nthread){
+    checkCudaErrors(cudaMalloc(&d_real, ndata*sizeof(TREAL)));
+    checkCudaErrors(cudaMalloc(&d_imag, ndata*sizeof(TIMAG)));
+
+    nblock = ndata/nthread;
+    nblock = (nblock>1)?nblock:1;
+    
+    cudautil_complexsplitter<<<nblock, nthread>>>(d_cmpx, d_real, d_imag, ndata);
+    getLastCudaError("Kernel execution failed [ cudautil_complexsplitter ]");
+  }
+  
+  //! Deconstructor of ComplexDataSplitter class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~ComplexDataSplitter(){
+    checkCudaErrors(cudaFree(d_real));
+    checkCudaErrors(cudaFree(d_imag));
+  }
+  
+private:
+  TCMPX *d_cmpx = NULL;
+  
+  int ndata;
+  int nthread;
+  int nblock;
+};
+
+//! A template kernel to calculate phase and amplitude of input array 
+/*!
+ * 
+ * \see scalar_typecast
+ *
+ * \tparam T Complex number component data type
+ * 
+ * \param[in]  v         Input real data which is the components of complex samples
+ * \param[in]  ndata     Number of data samples to be calculated
+ * \param[out] amplitude Calculated amplitude
+ * \param[out] phase     Calculated amplitude
+ *
+ */
+template <typename T>
+__global__ void cudautil_amplitude_phase(const T *v, float *amplitude, float *phase, int ndata){
+  int idx = blockDim.x*blockIdx.x + threadIdx.x;
+  
+  if(idx < ndata){
+    // We always do calculation in float
+    float v1;
+    float v2;
+    
+    scalar_typecast(v[2*idx],  v1);
+    scalar_typecast(v[2*idx+1],v2);
+    
+    amplitude[idx] = sqrtf(v1*v1+v2*v2);
+    phase[idx]     = atan2f(v2, v1); // in radians
+  }
+}
+
+template <typename T>
+class AmplitudePhaseCalculator{
+public:
+  float *d_amp = NULL;///< Calculated amplitude on device
+  float *d_pha = NULL;///< Calculated phase on device
+  
+  //! Constructor of AmplitudePhaseCalculator class.
+  /*!
+   * 
+   * - initialise the class
+   * - create device memory for amplitude and phase
+   * - calculate phase and amplitude with CUDA
+   *
+   * \see cudautil_amplitude_phase
+   *
+   * \tparam TIN Input data type
+   * 
+   * \param[in] d_data  Data to be calculated, it is interleaved as [REAL IMAG ... REAL IMAG] as type \p T
+   * \param[in] ndata   Number of samples to be converted, the size of d_data is 2*ndata
+   * \param[in] nthread Number of threads per CUDA block to run `cudautil_amplitude_phase` kernel
+   *
+   */
+  AmplitudePhaseCalculator(T *d_data,
+			   int ndata,
+			   int nthread
+			   )
+    :d_data(d_data), ndata(ndata), nthread(nthread){
+    // Get other buffers
+    checkCudaErrors(cudaMalloc(&d_amp, ndata * sizeof(float)));
+    checkCudaErrors(cudaMalloc(&d_pha, ndata * sizeof(float)));
+  
+    // Get amplitude and phase
+    nblock = ndata/nthread;
+    nblock = (nblock>1)?nblock:1;
+    cudautil_amplitude_phase<<<nblock, nthread>>>(d_data, d_amp, d_pha, ndata);
+  }
+  
+  //! Deconstructor of RealDataGeneratorNormal class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~AmplitudePhaseCalculator(){
+    
+    checkCudaErrors(cudaFree(d_amp));
+    checkCudaErrors(cudaFree(d_pha));
+  }
+
+private:
+  int ndata; ///< number of values as a private parameter
+  int nblock; ///< Number of CUDA blocks
+  int nthread; ///< number of threas per block
+  
+  T *d_data; ///< To get hold on the input data
+};
+
+
+/*! \brief A class to allocate memory on device 
+ *
+ * \tparam T data type of the device memory, which can be a complex data type
+ * 
+ */
+template <typename T>
+class DeviceMemoryAllocator {
+ public:
+  T *d_data = NULL; ///< Device memory
+  
+  /*! Constructor of class DeviceMemoryAllocator
+   *
+   * \param[in] ndata Number of data on host as type \p T
+   *
+   */
+ DeviceMemoryAllocator(int ndata)
+   :ndata(ndata){
+    checkCudaErrors(cudaMalloc(&d_data, ndata*sizeof(T)));
+  }
+  
+  //! Deconstructor of DeviceMemoryAllocator class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~DeviceMemoryAllocator(){
+    checkCudaErrors(cudaFree(d_data));
+  }
+
+ private:
+  int ndata; /// < Number of data points   
+};
+
+
+/*! \brief A class to copy device data to host
+ *
+ * \tparam T data type, which can be a complex data type
+ * 
+ */
+template <typename T>
+class DeviceDataExtractor {
+ public:
+  T *h_data = NULL; ///< Host buffer to hold data
+  
+  /*!
+   * \param[in] d_data Device data 
+   * \param[in] ndata Number of data on host as type \p T
+   * async memcpy will not work here as we always get new copy of memory
+   */
+ DeviceDataExtractor(T *d_data, int ndata)
+   :d_data(d_data), ndata(ndata){
+   
+    size = ndata*sizeof(T);
+    checkCudaErrors(cudaMallocHost(&h_data, size));
+    checkCudaErrors(cudaMemcpy(h_data, d_data, size, cudaMemcpyDeviceToHost));
+  }
+  
+  //! Deconstructor of DeviceDataExtractor class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~DeviceDataExtractor(){
+    checkCudaErrors(cudaFreeHost(h_data));
+  }
+  
+ private:
+  T *d_data = NULL;
+  int ndata;
+  int size;
+};
+
+
+/*! \brief A class to copy data from host to device
+ *
+ * \tparam T data type, which can be a complex data type
+ * 
+ */
+template <typename T>
+class HostDataExtractor {
+public:
+  T *d_data = NULL; ///< Device buffer to hold data
+
+  /*! Constructor of class HostDataExtractor
+   *
+   * \param[in] h_data Host data 
+   * \param[in] ndata Number of data on host as type \p T
+   * async memcpy will not work here as we always get new copy of memory
+   */
+  HostDataExtractor(T *h_data, int ndata)
+    :h_data(h_data), ndata(ndata){
+    
+    size = ndata*sizeof(T);
+    checkCudaErrors(cudaMalloc(&d_data, size));
+    checkCudaErrors(cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice));
+  }
+  
+  //! Deconstructor of HostDataExtractor class.
+  /*!
+   * 
+   * - free device memory at the class life end
+   */
+  ~HostDataExtractor(){
+    checkCudaErrors(cudaFree(d_data));
+  }
+  
+private:
+  T *h_data = NULL;
+  int ndata;
+  int size;
+};
+
+
+/*! \brief A class to allocate memory on host 
+ *
+ * \tparam T data type of the host memory, which can be a complex data type
+ * 
+ */
+template <typename T>
+class HostMemoryAllocator {
+ public:
+  T *h_data = NULL; ///< Host memory
+  
+  /*! Constructor of class HostMemoryAllocator
+   *
+   * \param[in] ndata Number of data on host as type \p T
+   *
+   */
+ HostMemoryAllocator(int ndata)
+   :ndata(ndata){
+    checkCudaErrors(cudaMallocHost(&h_data, ndata*sizeof(T)));
+  }
+  
+  //! Deconstructor of HostMemoryAllocator class.
+  /*!
+   * 
+   * - free host memory at the class life end
+   */
+  ~HostMemoryAllocator(){
+    checkCudaErrors(cudaFreeHost(h_data));
+  }
+
+ private:
+  int ndata; /// < Number of data points   
+};
+
+
 #endif
