5797f0150279e3abbdd484d4a3e007b12de3a21e
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2022-07-27 12:48:52 +1000

    more operator overload

--- include/cudautil.h
+++ include/cudautil.h
@@ -37,6 +37,8 @@ extern "C" {
 __device__ __host__ static inline cuComplex  operator*(cuComplex a, float b) { return make_cuComplex(a.x*b, a.y*b);}
 __device__ __host__ static inline cuComplex  operator*(float a, cuComplex b) { return make_cuComplex(b.x*a, b.y*a);}
 __device__ __host__ static inline cuComplex  operator/(cuComplex a, float b) { return make_cuComplex(a.x/b, a.y/b);}
+__device__ __host__ static inline cuComplex  operator+=(cuComplex a, cuComplex b) { return make_cuComplex(a.x+b.x, a.y+b.x);}
+__device__ __host__ static inline cuComplex  operator-=(cuComplex a, cuComplex b) { return make_cuComplex(a.x-b.x, a.y-b.x);}
 
 #include "reduction_kernel.h"
 #define CUDAUTIL_FLOAT2HALF __float2half
