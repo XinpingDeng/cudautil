346ff9a82bddf6bc1bd81d384baf7d5378071bcc
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2022-07-27 13:30:50 +1000

    divide operator looks okay with test code

--- tests/test.cu
+++ tests/test.cu
@@ -5,9 +5,19 @@
 #include "util.h"
 #include "cudautil.h"
 
+#include <iostream>
+
 int main(int argc, char *argv[]){
 
   print_cuda_memory_info();
+
+  float b = 3.0;
+  cuComplex a = {10.0, 10.0};
+
+  cuComplex c = a/b;
+
+  std::cout << c.x << "\t" 
+	    << c.y << std::endl;
   
   return EXIT_SUCCESS;
 }    
