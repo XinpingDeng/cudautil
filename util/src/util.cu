167ea17da014d70c983ae26f226594a5b5999719
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2022-08-01 10:24:05 +1000

    first header only version is done

--- util/src/util.cu
+++
@@ -1,6 +0,0 @@
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
-#include "util.cuh"
-#include "reduction.cuh"
--- util/util.h
+++ util/util.h
@@ -11,8 +11,26 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+  inline int seconds2dhms(uint64_t seconds, char *dhms){
+
+    uint64_t n = seconds;
   
-  int seconds2dhms(uint64_t seconds, char *dhms);
+    int days = n / (24 * 3600);
+  
+    n = n % (24 * 3600);
+    int hours = n / 3600;
+  
+    n %= 3600;
+    int minutes = n / 60 ;
+  
+    n %= 60;
+
+    sprintf(dhms, "%d:%02d:%02d:%02d", days, hours, minutes, (int)n);
+  
+    return EXIT_SUCCESS;
+  }
+  //int seconds2dhms(uint64_t seconds, char *dhms);
 
   // The following copied from https://stackoverflow.com/questions/41390824/%C2%B5s-precision-wait-in-c-for-linux-that-does-not-put-program-to-sleep
 # define tscmp(a, b, CMP)			\
