0b2c5c50f3296a91529a8f6ad8b4b8e33ac15752
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2022-08-26 13:00:02 +1000

    add sum2

--- utils/shared_utils.h
+++ utils/shared_utils.h
@@ -7,4 +7,18 @@
 
 bool approximates(const std::complex<float> &a, const std::complex<float> &b, unsigned nsamples);
 
+// Dichotomy way of accumulate an array on CPU and return the result
+template <typename T>
+static T Sum2(T *a, int lo, int hi){
+  // See here https://www.cnblogs.com/dx5800/p/13194664.html
+
+  if (lo==hi){
+    return a[lo];
+  }
+  
+  int mi = (lo + hi) / 2;
+
+  return Sum2(a, lo, mi) + Sum2(a, mi + 1, hi);
+}
+
 #endif // SHARED_UTILS_H
