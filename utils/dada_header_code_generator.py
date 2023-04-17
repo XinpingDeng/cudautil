2cba26a73d0e9482e9330726727325636f85a356
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2023-04-17 16:02:37 +1000

    backup

--- utils/dada_header_code_generator.py
+++ utils/dada_header_code_generator.py
@@ -9,22 +9,22 @@ from os.path import exists
 parser = argparse.ArgumentParser(
     description="Generate C code to get/set PSRDADA ascii header."
 )
-parser.add_argument("-j", "--json_file_name")
-parser.add_argument("-H", "--header_file_name")
-parser.add_argument("-s", "--source_file_name")
+parser.add_argument("-j", "--json_fname")
+parser.add_argument("-H", "--header_fname")
+parser.add_argument("-s", "--source_fname")
 
 args = parser.parse_args()
-json_file_name = args.json_file_name
-header_file_name = args.header_file_name
-source_file_name = args.source_file_name
+json_fname = args.json_fname
+header_fname = args.header_fname
+source_fname = args.source_fname
 
 # Create file handles for data read and write
-json_file = open(json_file_name, "r")
-dada_header = json.load(json_file)
+json_file = open(json_fname, "r")
+header = json.load(json_file)
 json_file.close()
 
 # First we need a header file
-header_file = open(header_file_name, "w")
+header_file = open(header_fname, "w")
 
 header_file.write("#ifndef __DADA_HEADER_H\n")
 header_file.write("#define __DADA_HEADER_H\n\n")
@@ -37,8 +37,8 @@ header_file.write("#endif\n\n")
 header_file.write('#include "inttypes.h"\n\n')
 
 header_file.write("  typedef struct dada_header_t{\n")
-for key in dada_header:
-    data_type = dada_header[key]
+for key in header:
+    data_type = header[key]
     print(key, data_type)
     if data_type == "string":
         header_file.write(f"    char {key.lower()}[DADA_STRLEN];\n")
@@ -48,17 +48,17 @@ for key in dada_header:
 header_file.write("  }dada_header_t;\n\n")
 
 header_file.write(
-    "  int read_dada_header(const char *dada_header_buffer, dada_header_t *dada_header);\n\n"
+    "  int read_dada_header(const char *buffer, dada_header_t *header);\n\n"
 )
 header_file.write(
-    "  int write_dada_header(const dada_header_t dada_header, char *dada_header_buffer);\n\n"
+    "  int write_dada_header(const dada_header_t header, char *buffer);\n\n"
 )
 
 header_file.write(
-    "  int read_dada_header_from_file(const char *dada_header_file_name, dada_header_t *dada_header);\n\n"
+    "  int read_dada_header_from_file(const char *fname, dada_header_t *header);\n\n"
 )
 header_file.write(
-    "  int write_dada_header_to_file(const dada_header_t dada_header,const char *dada_header_file_name);\n\n"
+    "  int write_dada_header_to_file(const dada_header_t header,const char *fname);\n\n"
 )
 
 header_file.write("#ifdef __cplusplus\n")
@@ -69,7 +69,7 @@ header_file.write("#endif\n")
 header_file.close()
 
 # Now we write source code
-source_file = open(source_file_name, "w")
+source_file = open(source_fname, "w")
 
 source_file.write("#ifndef _GNU_SOURCE\n")
 source_file.write("#define _GNU_SOURCE\n")
@@ -78,7 +78,7 @@ source_file.write("#endif\n\n")
 source_file.write('#include "futils.h"\n')
 source_file.write('#include "dada_def.h"\n')
 source_file.write('#include "ascii_header.h"\n\n')
-source_file.write(f'#include "{header_file_name}"\n\n')
+source_file.write(f'#include "{header_fname}"\n\n')
 
 source_file.write("#include <stdlib.h>\n")
 source_file.write("#include <stdio.h>\n\n")
@@ -87,29 +87,29 @@ source_file.write("#include <string.h>\n\n")
 # read from file function
 
 source_file.write(
-    "int read_dada_header_from_file(const char *dada_header_file_name, dada_header_t *dada_header){\n\n"
+    "int read_dada_header_from_file(const char *fname, dada_header_t *header){\n\n"
 )
 source_file.write(
-    "  char *dada_header_buffer = (char *)malloc(DADA_DEFAULT_HEADER_SIZE);\n"
+    "  char *buffer = (char *)malloc(DADA_DEFAULT_HEADER_SIZE);\n"
 )
-source_file.write("  memset(dada_header_buffer, 0, DADA_DEFAULT_HEADER_SIZE);\n\n")
+source_file.write("  memset(buffer, 0, DADA_DEFAULT_HEADER_SIZE);\n\n")
 source_file.write(
-    "  fileread(dada_header_file_name, dada_header_buffer, DADA_DEFAULT_HEADER_SIZE);\n"
+    "  fileread(fname, buffer, DADA_DEFAULT_HEADER_SIZE);\n"
 )
-source_file.write("  read_dada_header(dada_header_buffer, dada_header);\n\n")
-source_file.write("  free(dada_header_buffer);\n\n")
+source_file.write("  read_dada_header(buffer, header);\n\n")
+source_file.write("  free(buffer);\n\n")
 source_file.write("  return EXIT_SUCCESS;\n}\n\n")
 
 # read function
 source_file.write(
-    "int read_dada_header(const char *dada_header_buffer, dada_header_t *dada_header){\n\n"
+    "int read_dada_header(const char *buffer, dada_header_t *header){\n\n"
 )
 
-for key in dada_header:
-    data_type = dada_header[key]
+for key in header:
+    data_type = header[key]
     print(key, data_type)
 
-    # print(f'  if \(ascii_header_get\(dada_header_buffer, "{key}", "%d", dada_header.{key.lower\(\)}\) < 0\)  {\n')
+    # print(f'  if \(ascii_header_get\(buffer, "{key}", "%d", header.{key.lower\(\)}\) < 0\)  {\n')
     if data_type == "int":
         data_type_marker = '"%d"'
     if data_type == "float":
@@ -122,7 +122,7 @@ for key in dada_header:
         data_type_marker = '"%" PRIu64 ""'
 
     source_file.write(
-        f'  if (ascii_header_get(dada_header_buffer, "{key}", {data_type_marker}, &dada_header->{key.lower()}) < 0)'
+        f'  if (ascii_header_get(buffer, "{key}", {data_type_marker}, &header->{key.lower()}) < 0)'
     )
     source_file.write("  {\n")
     source_file.write(
@@ -139,32 +139,32 @@ source_file.write("  return EXIT_SUCCESS;\n}\n\n")
 
 # write to file function
 source_file.write(
-    "int write_dada_header_to_file(const dada_header_t dada_header, const char *dada_header_file_name){\n\n"
+    "int write_dada_header_to_file(const dada_header_t header, const char *fname){\n\n"
 )
-source_file.write('  FILE *fp = fopen(dada_header_file_name, "w");\n')
+source_file.write('  FILE *fp = fopen(fname, "w");\n')
 source_file.write(
-    "  char *dada_header_buffer = (char *)malloc(DADA_DEFAULT_HEADER_SIZE);\n"
+    "  char *buffer = (char *)malloc(DADA_DEFAULT_HEADER_SIZE);\n"
 )
-source_file.write("  memset(dada_header_buffer, 0, DADA_DEFAULT_HEADER_SIZE);\n\n")
+source_file.write("  memset(buffer, 0, DADA_DEFAULT_HEADER_SIZE);\n\n")
 source_file.write(
-    '  sprintf(dada_header_buffer, "HDR_VERSION  1.0\\nHDR_SIZE     4096\\n");\n'
+    '  sprintf(buffer, "HDR_VERSION  1.0\\nHDR_SIZE     4096\\n");\n'
 )
-source_file.write("  write_dada_header(dada_header, dada_header_buffer);\n")
-source_file.write('  fprintf(fp, "%s\\n", dada_header_buffer);\n\n')
-source_file.write("  free(dada_header_buffer);\n")
+source_file.write("  write_dada_header(header, buffer);\n")
+source_file.write('  fprintf(fp, "%s\\n", buffer);\n\n')
+source_file.write("  free(buffer);\n")
 source_file.write("  fclose(fp);\n\n")
 source_file.write("  return EXIT_SUCCESS;\n}\n\n")
 
 # write function
 source_file.write(
-    "int write_dada_header(const dada_header_t dada_header, char *dada_header_buffer){\n\n"
+    "int write_dada_header(const dada_header_t header, char *buffer){\n\n"
 )
 
-for key in dada_header:
-    data_type = dada_header[key]
+for key in header:
+    data_type = header[key]
     print(key, data_type)
 
-    # print(f'  if \(ascii_header_get\(dada_header_buffer, "{key}", "%d", dada_header.{key.lower\(\)}\) < 0\)  {\n')
+    # print(f'  if \(ascii_header_get\(buffer, "{key}", "%d", header.{key.lower\(\)}\) < 0\)  {\n')
     if data_type == "int":
         data_type_marker = '"%d"'
     if data_type == "float" or data_type == "double":
@@ -175,7 +175,7 @@ for key in dada_header:
         data_type_marker = '"%" PRIu64 ""'
 
     source_file.write(
-        f'  if (ascii_header_set(dada_header_buffer, "{key}", {data_type_marker}, dada_header.{key.lower()}) < 0)'
+        f'  if (ascii_header_set(buffer, "{key}", {data_type_marker}, header.{key.lower()}) < 0)'
     )
     source_file.write("  {\n")
     source_file.write(
