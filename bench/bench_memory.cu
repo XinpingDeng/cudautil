24227b6d991513526c598a24c8d46991517852d4
Deng, Xinping (S&A, Marsfield) <Xinping.Deng@csiro.au>
2023-06-14 14:49:20 +1000

    memory bench runs fine

--- bench/bench_memory.cu
+++ bench/bench_memory.cu
@@ -4,48 +4,74 @@
 
 #include "utils/cuda_utils.h"
 
-using namespace std;
+__global__ void krnl_add(const float *d_in1, float *d_in2, float *d_out, int ndata){
+  int idx = blockDim.x*blockIdx.x + threadIdx.x;
+  
+  if(idx < ndata){
+    d_out[idx] = d_in1[idx] + d_in2[idx];
+  }
+}
 
-// We can not compare converted numbers against CPU implementation
-// We can only check the difference between original data and converted data with mean and standard deviation
 int main(int argc, char *argv[]){
 
-  int ndata = 102400000;
-  float mean = 0;
+  int ndata    = 102400000;
+  float mean   = 0;
   float stddev = 10;
-  int nthread = 128;
+  int nthread  = 128;
+  int nrepeat  = 1000;
+  
+  std::cout << "ndata   " << ndata   << std::endl;
+  std::cout << "mean    " << mean    << std::endl;
+  std::cout << "stddev  " << stddev  << std::endl;
+  std::cout << "nthread " << nthread << std::endl;
+  std::cout << "nrepeat " << nrepeat << std::endl;
 
+  uint64_t nbyte = ndata*sizeof(float);
+  int nblock = ndata/nthread;
+  
+  std::cout << "buffer size is " << nbyte/1E9 << "Gbyte" << std::endl;
+  std::cout << "nblock is      " << nblock << std::endl;
+  
+  // Get float data 
+  curandGenerator_t gen;
+  checkCudaErrors(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
+  checkCudaErrors(curandSetPseudoRandomGeneratorSeed(gen, time(NULL)));
+  RealGeneratorNormal data(gen, mean, stddev, ndata);
+  print_cuda_memory_info();
+
+  // get device memory space
+  float *d_in1 = NULL;
+  float *d_in2 = NULL;
+  float *d_out = NULL;
+  checkCudaErrors(cudaMalloc(&d_in1, nbyte));
+  checkCudaErrors(cudaMalloc(&d_in2, nbyte));
+  checkCudaErrors(cudaMalloc(&d_out, nbyte));
+
+  // setup device memory
+  checkCudaErrors(cudaMemcpy(d_in1, data.data, nbyte, cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(d_in2, data.data, nbyte, cudaMemcpyHostToDevice));
+  
+  // setup timer 
   cudaEvent_t g_start;
   cudaEvent_t g_stop;
   float gtime = 0;
   checkCudaErrors(cudaEventCreate(&g_start));
   checkCudaErrors(cudaEventCreate(&g_stop));
+  
   CUDA_STARTTIME(g);
-  
-  // Get float data
-  curandGenerator_t gen;
-  checkCudaErrors(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
-  checkCudaErrors(curandSetPseudoRandomGeneratorSeed(gen, time(NULL)));
-  RealGeneratorNormal normal_data(gen, mean, stddev, ndata);
-  print_cuda_memory_info();
+  for(int i = 0; i < nrepeat; i++){
+    krnl_add<<<nblock, nthread>>>(d_in1, d_in2, d_out, ndata);
+    getLastCudaError("Kernel execution failed [ krnl_add ]");
 
-  // Convert to float
-  RealConvertor<float, float> normal_data_float(normal_data.data, ndata, nthread);
-
-  // Get the difference
-  RealDifferentiator<float, float> normal_data_diff(normal_data.data, normal_data_float.data, ndata, nthread);
-  
-  // Get mean and standard deviation
-  RealMeanStddevCalculator<float> mean_stddev(normal_data_diff.data, ndata, nthread, 7);
+  }
   
   CUDA_STOPTIME(g);
-  cout << "elapsed time is " << gtime << " milliseconds" << endl;
+  std::cout << "elapsed time is " << gtime/nrepeat << " milliseconds" << std::endl;
 
-  float mean_f = mean_stddev.mean;
-  float stddev_f = mean_stddev.stddev ;
-  cout << "mean is " << mean_f << " "
-       << "stddev is " << stddev_f 
-       << endl;
-
+  // free memory
+  checkCudaErrors(cudaFree(d_in1));
+  checkCudaErrors(cudaFree(d_in2));
+  checkCudaErrors(cudaFree(d_out));
+  
   return EXIT_SUCCESS;
 }
